4be7926b1dbba5901362250960968d26ef8fe5d0
jeperico <joaoeperico@gmail.com>
2026-07-22 10:06:16 -0300

    chore: update favicon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -18,6 +18,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Last Island",
   description: "Multiplayer naval battle game with One Piece theming",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
